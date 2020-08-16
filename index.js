f84620272bab0af40e037ce91251214ebdc390b7
Dmytro Agafonov <dmytro.agafonov@abraxas.ch>
2020-08-16 19:40:43 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const Telegraf = require('telegraf');
+const app = new Telegraf(process.env.BOT_TOKEN);
+
+app.hears('hi', ctx => {
+ return ctx.reply('Hey!');
+});
+
+app.command('start', ctx => {
+ console.log(JSON.stringify(ctx));
+});
+
+app.startPolling();
